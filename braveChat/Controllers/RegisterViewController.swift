39cbcdb1d1a19269cfca2ed301e6b4cc7cc38277
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-03-09 04:53:41 +0400

    main: first implementation of Firebase Auth, added .gitignore

--- braveChat/Controllers/RegisterViewController.swift
+++ braveChat/Controllers/RegisterViewController.swift
@@ -1,11 +1,36 @@
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            if email.isValidEmail {
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    if let e = error {
+                        print(e.localizedDescription)
+                    } else {
+                        self.performSegue(withIdentifier:"RegisterToChat", sender: self)
+                    }
+                }
+            } else {
+                print("Incorrect email")
+            }
+        }
+        
+        
+        
+        
     }
     
+}
+
+extension String {
+    var isValidEmail: Bool {
+        NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
+    }
 }
